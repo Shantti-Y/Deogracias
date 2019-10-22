18f72618fc2ef9cf81cdba3956be147d8613c084
Takahiro Yoshioka <shanttiy@Takahiros-MacBook-Pro.local>
2019-10-23 00:27:33 +0900

    create tag saga and action in redux

--- src/store/action/entity/tag.ts
+++ src/store/action/entity/tag.ts
@@ -3,6 +3,15 @@ import { createAction } from 'redux-actions';
 export const FETCH_TAGS = 'entity/tag/FETCH_TAGS';
 export const fetchTags = createAction(FETCH_TAGS);
 
+export const CREATE_TAG = 'entity/tag/CREATE_TAG';
+export const createTag = createAction(CREATE_TAG);
+
+export const UPDATE_TAG = 'entity/tag/UPDATE_TAG';
+export const updateTag = createAction(UPDATE_TAG);
+
+export const DELETE_TAG = 'entity/manga/DELETE_TAG';
+export const deleteTag = createAction(DELETE_TAG);
+
 // for calling reducers
 export const SET_TAGS = 'entity/tag/SET_TAGS';
 export const setTags = createAction(SET_TAGS);
--- src/store/saga/entity/tag.ts
+++ src/store/saga/entity/tag.ts
@@ -4,18 +4,28 @@ import { TableName, appDB } from '@util/database';
 
 import {
   FETCH_TAGS,
+  CREATE_TAG,
+  UPDATE_TAG,
+  DELETE_TAG,
+  fetchTags,
   setTags
 } from '@action/entity/tag';
 
 // APIs
-async function* invokeFetchTags(action) {
-  const records = await appDB[TableName.Tags].toArray();
-  yield put(setTags(records));
+function* invokeFetchTags(action) {
+  yield(appDB[TableName.Tags].toArray(result => put(setTags(result))));
+}
+function* invokeCreateTag(action) {
+  const { tag } = action.payload;
+  yield (appDB[TableName.Tags])
+    .add(tag)
+    .then(() => put(fetchTags()));
 }
 
 // Bundle api functions to watcher and saga
 function* watchAsyncTriggers() {
   yield takeLatest(FETCH_TAGS, invokeFetchTags);
+  yield takeLatest(CREATE_TAG, invokeCreateTag);
 }
 
 export default function* saga() {
