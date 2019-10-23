6c4ea35ce7debebe032dadba24089499ae7452cc
Takahiro Yoshioka <shanttiy@Takahiros-MacBook-Pro.local>
2019-10-24 07:39:24 +0900

    change editor drawer structure

--- src/component/DrawerItem/MangaEditor/index.tsx
+++ src/component/DrawerItem/MangaEditor/index.tsx
@@ -1,44 +1,23 @@
 import React, { FC, useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-import ItemContainer from '@component/DrawerItem/ItemContainer';
+import MenuItem from '@component/DrawerItem/MenuItem';
+import NewTagItem from '@component/DrawerItem/NewTagItem';
+import TagList from '@component/DrawerItem/TagList';
 
 import './style.scss';
 
-interface MangaEditorProps {
-  tags: TagEntity[]
-}
-const MangaEditor: FC<MangaEditorProps> = props => {
-
-  const menuItems = () => {
-    const newMangaMenu = {
-      label: 'New Manga',
-      icon: 'pi pi-fw pi-upload',
-    };
-    const newTagMenu = {
-      label: 'New Tag',
-      icon: 'pi pi-fw pi-bookmark'
-    };
-    const tagList = {
-      label: 'Tags',
-      icon: 'pi pi-fw pi-tags',
-      items: props.tags.length > 0 ?
-        props.tags.map(tag => {
-          return { label: tag.name }
-        }) :
-        [{ label: 'No Tag' }]
-    }
-    return [newMangaMenu, newTagMenu, tagList]
-  }
+interface ComponentProps {}
+const MangaEditor: FC<ComponentProps> = props => {
+  const history = useHistory();
 
   return (
-    <ItemContainer
-      menuItems={menuItems()}
-    />
+    <div className="drawer-manga-editor drawer-menu-list">
+      <MenuItem icon="pi pi-home" text="Go to Home" onClick={() => history.push(`/`)} />
+      <NewTagItem />
+      <TagList />
+    </div>
   )
 }
-const mapStateToProps = state => ({
-  tags: state.entity.tag.tags
-});
 
-export default connect(mapStateToProps, null)(MangaEditor);
+export default MangaEditor;
