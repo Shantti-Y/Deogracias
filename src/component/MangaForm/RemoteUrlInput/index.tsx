c2a5055a315cb9a889e41160c363d11bfbcce0f0
Takahiro Yoshioka <shanttiy@Takahiros-MacBook-Pro.local>
2019-10-27 20:48:43 +0900

    enable to fetch external urls massively with short settings

--- src/component/MangaForm/RemoteUrlInput/index.tsx
+++ src/component/MangaForm/RemoteUrlInput/index.tsx
@@ -3,34 +3,96 @@ import React, { FC, useState } from 'react';
 import './style.scss';
 
 import { InputTextarea } from 'primereact/inputtextarea';
+import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 
 interface RemoteUrlInputProps {
   onSubmit: (imagePaths: string[]) => void
 }
+interface RemoteSiteCrawlerSetting {
+  siteUrl: string;
+  imageSelector: string;
+  nextLinkSelector: string;
+  limitLinks: number
+}
 const RemoteUrlInput: FC<RemoteUrlInputProps> = props => {
-  const [imageUrlText, setImageUrlText] = useState('' as string);
+  const [imageUrlInlineText, setImageUrlInlineText] = useState('' as string);
+  const [remoteSiteCrawlerSetting, setRemoteSiteCrawlerSetting] = useState({
+    siteUrl: '',
+    imageSelector: '',
+    nextLinkSelector: '',
+    limitLinks: 0
+  } as RemoteSiteCrawlerSetting);
+  const [fetchingRemoteUrls, setFetchingRemoteUrls] = useState(false);
 
-  const handleChanged = (event: React.FormEvent) => {
+  const handleInlineTextChanged = (event: React.FormEvent) => {
     const target = event.target as HTMLTextAreaElement;
-    setImageUrlText(target.value);
+    setImageUrlInlineText(target.value);
   }
 
-  const handleSubmitted = () => {
-    const imageUrls = imageUrlText.split(/\n/);
+  const handleInlineSubmitted = () => {
+    const imageUrls = imageUrlInlineText.split(/\n/);
     props.onSubmit(imageUrls);
-    setImageUrlText('');
+    setImageUrlInlineText('');
+  }
+
+  const handleCrawlerSettingChanged = (name: string, event: React.FormEvent) => {
+    const target = event.target as HTMLInputElement;
+    setRemoteSiteCrawlerSetting({
+      ...remoteSiteCrawlerSetting,
+      [name]: target.value
+    });
+  }
+
+  const handleCrawlingSettingSubmitted = async () => {
+    if (!fetchingRemoteUrls){
+      setFetchingRemoteUrls(true);
+      const fetchResult = await fetch('http://localhost:3000/remote-site-images', {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(remoteSiteCrawlerSetting)
+      }).then((response) => response.json());
+      setFetchingRemoteUrls(false);
+      props.onSubmit(fetchResult.imageUrls);
+    }
   }
 
   return (
     <div>
       RemoteImageSetting
-      <InputTextarea
-        value={imageUrlText}
-        autoResize={true}
-        onChange={event => handleChanged(event)}
-      />
-      <Button label="Add Urls" onClick={handleSubmitted} />
+
+      <div>
+        <InputTextarea
+          value={imageUrlInlineText}
+          autoResize={true}
+          onChange={event => handleInlineTextChanged(event)}
+        />
+        <Button label="Add Urls" onClick={handleInlineSubmitted} />
+      </div>
+      <div>
+        <InputText
+          value={remoteSiteCrawlerSetting.siteUrl}
+          onChange={event => handleCrawlerSettingChanged('siteUrl', event)}
+          placeholder="site url"
+        />
+        <InputText
+          value={remoteSiteCrawlerSetting.imageSelector}
+          onChange={event => handleCrawlerSettingChanged('imageSelector', event)}
+          placeholder="image selector (e.g. body > a > image)"
+        />
+        <InputText
+          value={remoteSiteCrawlerSetting.nextLinkSelector}
+          onChange={event => handleCrawlerSettingChanged('nextLinkSelector', event)}
+          placeholder="next link selector (e.g. body > a)"
+        />
+        <InputText
+          value={remoteSiteCrawlerSetting.limitLinks}
+          onChange={event => handleCrawlerSettingChanged('limitLinks', event)}
+          placeholder="limit link number"
+        />
+        <Button label="Get Images From Website" onClick={handleCrawlingSettingSubmitted} />
+      </div>
     </div>
     
   );
